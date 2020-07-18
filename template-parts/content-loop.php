f6091f57d9061b6ff74d9a2b822498d9ce0f6173
Kasutan <contact@kasutan.pro>
2020-07-18 17:27:07 +0200

    Adapter content loop pour résultats de recherche

--- template-parts/content-loop.php
+++ template-parts/content-loop.php
@@ -13,10 +13,17 @@
 		<?php
 			echo get_the_category_list();
 
-			printf('<a class="image" href="%s"><div class="image-wrapper">%s</div></a>',
+			if(has_post_thumbnail()) :
+				printf('<a class="image" href="%s"><div class="image-wrapper">%s</div></a>',
+					get_permalink(),
+					get_the_post_thumbnail( get_the_ID(), 'medium')
+				);
+			else : 
+				printf('<a class="image" href="%s"><div class="image-wrapper"><img src="%s" alt="Logo My Big Bang" width="170" height="170"/></div></a>',
 				get_permalink(),
-				get_the_post_thumbnail( get_the_ID(), 'medium')
+				mbb_get_picto_url('logo-Footer') 
 			);
+			endif;
 
 			printf('<span class="date">%s</span>', get_the_date(''));
 
