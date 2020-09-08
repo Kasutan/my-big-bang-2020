071bf923a7681f3aa05d466596ecea5a48028a96
Kasutan <contact@kasutan.pro>
2020-09-08 11:18:39 +0200

    Corriger erreur de syntaxe GM

--- functions.php
+++ functions.php
@@ -282,11 +282,7 @@ function fdc_add_body_start() {
 	<!-- End Google Tag Manager (noscript) -->
 
 	<!-- Google Tag Manager -->
-	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({"gtm.start":
-	new Date().getTime(),event:"gtm.js"});var f=d.getElementsByTagName(s)[0],
-	j=d.createElement(s),dl=l!="dataLayer"?""&l=""+l:"";j.async=true;j.src=
-	"https://www.googletagmanager.com/gtm.js?id="+i+dl;f.parentNode.insertBefore(j,f);
-	})(window,document,"script","dataLayer","GTM-M25XWT");</script>
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({"gtm.start":new Date().getTime(),event:"gtm.js"});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!="dataLayer"?"&l="+l:"";j.async=true;j.src="https://www.googletagmanager.com/gtm.js?id="+i+dl;f.parentNode.insertBefore(j,f);})(window,document,"script","dataLayer","GTM-M25XWT");</script>
 	<!-- End Google Tag Manager -->	';
 
 }
